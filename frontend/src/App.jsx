517b87bc37d70c4ee1002ea15a7723b547f33c55
PedroLucasDeveloper <spedrolucas740@gmail.com>
2024-07-17 11:16:33 -0300

    Adicionando rotas

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,17 @@
 import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import PaginaInicial from './pages/PaginaInicial'
 
 function App() {
 
   return (
-    <>
-
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<PaginaInicial />} />
+      </Routes>
+    </Router>
   )
 }
 
 export default App
+
