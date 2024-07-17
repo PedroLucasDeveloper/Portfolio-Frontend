5774efe02efd826f730a6e650c88d85a173619d1
PedroLucasDeveloper <spedrolucas740@gmail.com>
2024-07-17 10:11:27 -0300

    Initial Commit

---
+++ frontend/src/pages/PaginaInicial.jsx
@@ -0,0 +1,12 @@
+import { Box, Heading } from '@chakra-ui/react'
+
+function PaginaInicial() {
+    
+  return (
+    <Box>
+        <Heading>Página Inicial</Heading>
+    </Box>
+  )
+}
+
+export default PaginaInicial
---
+++ frontend/src/pages/Projetos.jsx
@@ -0,0 +1,11 @@
+function Projetos() {
+
+    return (
+      <>
+  
+      </>
+    )
+  }
+  
+  export default Projetos
+  
