a3f3c2c9178967262d960c89ac9a34eabe35ffe8
PedroLucasDeveloper <spedrolucas740@gmail.com>
2024-08-06 09:27:07 -0300

    Algumas melhorias

--- frontend/src/components/Hello.jsx
+++ frontend/src/components/Hello.jsx
@@ -1,26 +1,55 @@
-import { Box, Button, Grid, GridItem, Heading, Stack, Text } from '@chakra-ui/react'
+import { Box, Button, Grid, GridItem, Heading, Stack, Text, useBreakpointValue } from '@chakra-ui/react';
 
 function Hello() {
-    
-  return (
-    <Box h={'500px'}>
-      <Grid templateColumns='repeat(2, 1fr)' gap={2}>
-      <GridItem display={'flex'} justifyContent={'center'} alignItems={'center'} w='100%' p={3}>
-        <Heading fontSize={'50px'}>Olá mundo! <br /> Eu sou Pedro Lucas :)</Heading>
-      </GridItem>
+  const gridTemplateColumns = useBreakpointValue({ base: '1fr', md: 'repeat(2, 1fr)' });
 
-      <GridItem display={'flex'} flexDirection={'column'} justifyContent={'center'} alignItems={'center'} w='100%' h={'40vh'} p={3}>
-      <Heading fontSize={'20px'} mb={4}>A Website developer based in Quixadá, Brasil.</Heading>
-      <Text mb={3} color={'gray'}>blabla blablablaaaaaaaaaaaaaaaaaaaaaaa</Text>
-        <Stack spacing={8} direction={'row'}>
-            <Button bg={'black'} color={'white'} w={'130px'}>Talk to me</Button>
-            <Button variant={'solid'} border={'1px solid lightgray'} bg={'white'} w={'130px'}>See my work</Button>
-        </Stack>
-      </GridItem> 
-    </Grid>
-      
+  return (
+    <Box h={{ base: 'auto', md: '500px' }} p={4}>
+      <Grid templateColumns={gridTemplateColumns} gap={4}>
+        <GridItem
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          p={4}
+          textAlign="center"
+        >
+          <Heading
+            fontFamily="Luckiest Guy, sans-serif"
+            color="black"
+            textShadow="3px 3px 4px lightgray"
+            fontWeight="normal"
+            fontSize={{ base: '36px', md: '60px' }}
+          >
+            Olá mundo! <br /> Eu sou Pedro Lucas :)
+          </Heading>
+        </GridItem>
+
+        <GridItem
+          display="flex"
+          flexDirection="column"
+          justifyContent="center"
+          alignItems="center"
+          p={4}
+          textAlign="center"
+        >
+          <Heading fontSize={{ base: '18px', md: '20px' }} mb={4}>
+            A Website developer based in Quixadá, Brasil.
+          </Heading>
+          <Text mb={3} color="gray">
+            blabla blablablaaaaaaaaaaaaaaaaaaaaaaa
+          </Text>
+          <Stack spacing={4} direction={{ base: 'column', md: 'row' }}>
+            <Button bg="black" color="white" w={{ base: 'full', md: '130px' }}>
+              Talk to me
+            </Button>
+            <Button variant="outline" borderColor="lightgray" color="black" w={{ base: 'full', md: '130px' }}>
+              See my work
+            </Button>
+          </Stack>
+        </GridItem>
+      </Grid>
     </Box>
-  )
+  );
 }
 
-export default Hello
+export default Hello;
